7935c03821243ecb2bf75ed46bd94a4fcb479f7f
Freezman <Freezman31@users.noreply.github.com>
2022-01-26 20:32:11 +0100

    Fix missing import

--- lib/core/src/api/apis/kdecole/kdecole.dart
+++ lib/core/src/api/apis/kdecole/kdecole.dart
@@ -2,6 +2,7 @@ library kdecole;
 
 import 'dart:math';
 
+import 'package:http/src/request.dart';
 import 'package:kdecole_api/kdecole_api.dart' as k;
 import 'package:ynotes/core/api.dart';
 import 'package:ynotes/core/src/utilities/app_colors.dart';
